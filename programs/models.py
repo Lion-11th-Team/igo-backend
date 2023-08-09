961ce1b87219dad97fadda3e97a0decadcccf4ab
Jiseok Son <jiseokson@gmail.com>
2023-08-09 14:02:47 +0900

    fix: program model 필드와 뷰
    -활동영역을 선택하는 activity_category 필드 추가
    -등록가능 상태를 표현하는 is_registing을 property 필드로 변경
    -응답 본문의 등록 상태 status를 'before', 'now', 'after' 3 가지 중 하나로 평가

--- programs/models.py
+++ programs/models.py
@@ -2,6 +2,12 @@ from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.db import models
 
+ACTIVITY_CATEGORY_CHOICES = (
+    ('kiosk', '키오스크'),
+    ('app_payment', '앱 결제'),
+    ('bank_transfer', '계좌 이체'),
+)
+
 
 class Program(models.Model):
     title = models.CharField(max_length=256)
@@ -18,24 +24,19 @@ class Program(models.Model):
     activity_start_at = models.DateTimeField()
     activity_end_at = models.DateTimeField()
 
-    is_registing = models.BooleanField(default=True)
-    is_active = models.BooleanField(default=False)
-
     subscriber_limit = models.IntegerField()
     subscriber_num = models.IntegerField(default=0)
     subscriber = models.ManyToManyField(
         to=get_user_model(), related_name='program')
-    
-    activity_field_choices = (
-        ('kiosk', '키오스크'),
-        ('app_payment', '앱 결제'),
-        ('bank_transfer', '계좌 이체'),
-        # 다른 분야도 추가 예정
-    )
 
-    activity_field = models.CharField(max_length=50, choices=activity_field_choices)
-    area = models.CharField(max_length=50)
+    activity_category = models.ManyToManyField(
+        choices=ACTIVITY_CATEGORY_CHOICES)
+    # location = models.CharField(max_length=50)
 
     def save(self, *args, **kwargs):
         self.updated_at = timezone.now()
         super().save(*args, **kwargs)
+
+    @property
+    def is_registing(self):
+        return self.regist_start_at <= timezone.now() <= self.regist_end_at and self.subscriber.count < self.subscriber_limit
