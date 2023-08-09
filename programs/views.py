b80ce0122c0572b638e10597953c84c72f032a2c
Jiseok Son <jiseokson@gmail.com>
2023-08-10 00:19:44 +0900

    fix: program 신청 권한 확인 기능

--- programs/views.py
+++ programs/views.py
@@ -1,28 +1,32 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from django.utils import timezone
 
 from programs.models import Program
 from programs.serializers import ProgramSerializer
-from .permissions import IsCarerOrReadOnly, IsStudent
+from .permissions import IsCarer, IsProgramAuthor, IsStudent
 
 
 class ProgramViewSet(ModelViewSet):
     queryset = Program.objects.order_by('-created_at')
     serializer_class = ProgramSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsCarerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-    
+
     def get_permissions(self):
         if self.action == 'subscribe':
-            self.permission_classes = [IsStudent]
-        return super().get_permissions()
-
+            permission_classes = [IsAuthenticated, IsStudent]
+        elif self.action in ['create']:
+            permission_classes = [IsAuthenticated, IsCarer]
+        elif self.action in ['update', 'partial_update', 'delete']:
+            permission_classes = [IsAuthenticated, IsCarer, IsProgramAuthor]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
 
     @action(detail=True, methods=('POST', 'DELETE'))
     def subscribe(self, request, *args, **kwargs):
