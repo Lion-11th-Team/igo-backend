1c98512f5ea928153029cb99a2cbdeb8787d5a2b
DESKTOP-KLIL89F <dain0928@g.hongik.ac.kr>
2023-08-10 22:23:29 +0900

    Add: donation view & router

--- donations/views.py
+++ donations/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny, IsAdminUser
 
-# Create your views here.
+from .models import Donation
+from .serializers import DonationSerializer
+
+class DonationViewSet(ModelViewSet):
+    queryset = Donation.objects.all()
+    serializer_class = DonationSerializer
+
+    def get_permissions(self):
+        if self.action in ['create']:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+    
