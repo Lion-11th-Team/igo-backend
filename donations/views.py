f8cf6c72e7500b464c6bcc8b390f0f2208b0ccc5
unam17 <unam6604@gmail.com>
2023-08-11 21:17:30 +0900

    도네이션 뷰 작성 추가

--- donations/views.py
+++ donations/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.response import Response
 
 from .models import Donation
 from .serializers import DonationSerializer
+from rest_framework.decorators import action
 
 class DonationViewSet(ModelViewSet):
     queryset = Donation.objects.all()
@@ -16,3 +18,8 @@ class DonationViewSet(ModelViewSet):
             permission_classes = [IsAdminUser]
         return [permission() for permission in permission_classes]
     
+    @action(detail=True, methods=('POST',))
+    def donations(self, request, *args, **kwargs):
+        donation_record= DonationSerializer(data=request.data)
+        donation_record.save()
+        return Response(donation_record.data)
