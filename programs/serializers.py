f34dac1d733b3e307a60207aa9fca7f8de47e5a1
Jiseok Son <jiseokson@gmail.com>
2023-08-17 21:06:50 +0900

    add: program response에 요양원 이름 추가

--- programs/serializers.py
+++ programs/serializers.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
+from profiles.models import CarerProfile
 from profiles.serializers import AddressSerializer
 
 from programs.models import Program
@@ -11,6 +12,7 @@ from programs.models import Program
 class ProgramSerializer(ModelSerializer):
     author = serializers.ReadOnlyField(source='author.pk')
     subscriber = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    facility_name = serializers.SerializerMethodField()
 
     activity_status = serializers.ReadOnlyField()
     regist_status = serializers.ReadOnlyField()
@@ -19,3 +21,7 @@ class ProgramSerializer(ModelSerializer):
     class Meta:
         model = Program
         fields = '__all__'
+
+    def get_facility_name(self, obj):
+        profile = CarerProfile.objects.get(user=obj.author)
+        return profile.facility_name
