4b746a04f61f6538cf737f917fa407eb97d33393
Jiseok Son <jiseokson@gmail.com>
2023-08-18 19:25:14 +0900

    add: donation 주소

--- donations/models.py
+++ donations/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 
+from profiles.models import Address
+
 DONATION_MODEL_CATEGORY = (
     ('smartphone', '스마트폰'),
     ('tablet', '태블릿'),
     ('laptop', '노트북'),
 )
 
+
 class Donation(models.Model):
-    model_category = models.CharField(max_length=20, choices=DONATION_MODEL_CATEGORY)
+    model_category = models.CharField(
+        max_length=20, choices=DONATION_MODEL_CATEGORY)
     model_name = models.CharField(max_length=256)
     remarks = models.CharField(max_length=256)
 
     sender_name = models.CharField(max_length=256)
     sender_phone = models.CharField(max_length=64)
-    sender_address = models.CharField(max_length=256)
-    content = models.CharField(max_length=256)
+    sender_address = models.ForeignKey(
+        to=Address, related_name='donation', on_delete=models.CASCADE)
--- donations/serializers.py
+++ donations/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework.serializers import ModelSerializer
+
+from profiles.models import Address
 from .models import Donation
+
 
 class DonationSerializer(ModelSerializer):
     class Meta:
         model = Donation
         fields = '__all__'
+
+    def create(self, validated_data):
+        address_data = validated_data.pop('address')
+        address_inst = Address.objects.create(**address_data)
+        donation_inst = Donation.objects.create(
+            **validated_data, address=address_inst)
+        return donation_inst
