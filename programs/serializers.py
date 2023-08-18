d23b7d69fd24ff2ef24c0d37b5c05a205c0ae253
Jiseok Son <jiseokson@gmail.com>
2023-08-18 18:41:51 +0900

    add: program response에 전화, 이메일 포함

--- programs/serializers.py
+++ programs/serializers.py
@@ -13,6 +13,8 @@ class ProgramSerializer(ModelSerializer):
     author = serializers.ReadOnlyField(source='author.pk')
     subscriber = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     facility_name = serializers.SerializerMethodField()
+    phone = serializers.SerializerMethodField()
+    email = serializers.SerializerMethodField()
 
     activity_status = serializers.ReadOnlyField()
     regist_status = serializers.ReadOnlyField()
@@ -21,6 +23,14 @@ class ProgramSerializer(ModelSerializer):
     class Meta:
         model = Program
         fields = '__all__'
+
+    def get_phone(self, obj):
+        profile = CarerProfile.objects.get(user=obj.author)
+        return profile.phone
+
+    def get_email(self, obj):
+        profile = CarerProfile.objects.get(user=obj.author)
+        return profile.email
 
     def get_facility_name(self, obj):
         profile = CarerProfile.objects.get(user=obj.author)
